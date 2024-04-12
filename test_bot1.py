c805e8d96d4c7d79ccfcc836d44afb6ac0576183
Abdulaziz742 <166836256+Abdulaziz742@users.noreply.github.com>
2024-04-12 19:26:25 +0500

    Update test_bot1.py
    version2

--- test_bot1.py
+++ test_bot1.py
@@ -19,3 +19,46 @@ def start(message):
     bot.send_message(message.chat.id, 'Username kriting')
 
     bot.register_next_step_handler(message, user_name)
+
+
+name = ''
+
+
+def user_name(message):
+    global name
+    name = message.text.strip()
+    bot.send_message(message.chat.id, "Paro'lni kiriting")
+    bot.register_next_step_handler(message, user_password)
+
+
+def user_password(message):
+    password = message.text.strip()
+    conn = sqlite3.connect('test_bot.db')
+    cur = conn.cursor()
+    data = (f"{name}", F"{password}")
+    cur.execute(f"INSERT INTO user (name, password) VALUES {data}")
+    conn.commit()
+    cur.close()
+    conn.close()
+    markup = telebot.types.InlineKeyboardMarkup()
+    markup.add(telebot.types.InlineKeyboardMarkup("Userlar royxati", callblack_data="user"))
+    bot.send_message(message.chat.id, "Royxatdan otildi", reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback(call, el=None):
+    conn = sqlite3.connect('test_bot.db')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM user")
+    user = cur.fetchone()
+    info = ''
+    for i in user:
+        info += f"ism{el[1]} Parol {el[2]}"
+
+    cur.close()
+    conn.close()
+    bot.send_message(call.message.chat.id, info)
+
+
+print("The bot is running...")
+bot.polling()
+
